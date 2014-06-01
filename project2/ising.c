9343ebb1f357ded9e4c02b57c71010c6dec7d224
alden <adderan@ucsc.edu>
2014-05-31 22:37:08 -0700

    -

--- project2/ising.c
+++ project2/ising.c
@@ -1,53 +1,82 @@
-#include <rand.h>;
-#include <math.h>;
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
-double J = 1.0;
-double k = 1.38*pow(10, -23);
+#define J 1.0
+#define k 1.38*pow(10, -23)
 
-int main() {
-	srand(time(NULL));
-	int[][] lattice = calloc(L*L, sizeof(int));
+
+double magnetization(int **lattice, int L) {
+	double m = 0;
 	int i,j;
 	for(i = 0; i < L; i++) {
 		for(j = 0; j < L; j++) {
-			lattice[i][j] = 1;
-		}
-	
-	}
-
-
-}
-
-	
-void sweep(int[][] lattice, double T, int L) {
-	double B = 1.0/(k*T); 
-	int i, j;
-
-	for(i = 0; i < L; i++) {
-		for(j = 0; j < L; j++) {
-			double r = rand()/ (RAND_MAX + 1.0);
-			int[] neighbors = calloc(4, sizeof(int));
-			neighbors[0] = lattice[i][j+1];
-			neighbors[1] = lattice[i][j-1];
-			neighbors[2] = lattice[i+1][j];
-			neighbors[3] = lattice[i-1][j];
-			int Si = neighbors[i][j];
-			double met = exp(-1*B*energy(neighbors[]. Si));
-
-			if(r < met) {
-				neighbors[i][j] *= -1.0;
-			}
-
+			m += lattice[i][j];
 		}
 	}
+	return m/(L*L);
 }
-	
-double energy(int[] neighbors, int Si) {
+		
+double energy(int neighbors[], int Si) {
 	double e = 0.0;
 	int i;
 	for(i = 0; i < 3; i++) {
 		e += (-2.0)*J*Si*neighbors[i];
 	}
 	return e;
+}
+
+void sweep(int L, int **lattice, double T) {
+	double B = 1.0/(k*T); 
+	int ip, jp;
+	int im = L-2;
+	int jm = L-2;
+	int i = L-1;
+	int j = L-1;
+	for(ip = 0; ip < L; ip++) {
+		for(jp = 0; jp < L; jp++) {
+			double r = rand()/ (RAND_MAX + 1.0);
+			int neighbors[4];
+			neighbors[0] = lattice[i][jp];
+			neighbors[1] = lattice[i][jm];
+			neighbors[2] = lattice[ip][j];
+			neighbors[3] = lattice[im][j];
+			int Si = lattice[i][j];
+			double met = exp(-1*B*energy(neighbors, Si));
+
+			if(r < met) {
+				lattice[i][j] *= -1.0;
+			}
+			im = i;
+			jm = j;
+			i = ip;
+			j = jp;
+
+
+		}
+	}
+}
+
+int main() {
+	double T = 100.0;
+	srand(time(NULL));
+	int L = 10;
+	int **lattice = (int**)malloc(sizeof(int*)*L);
+	int i,j;
+	for(i = 0; i < L; i++) {
+		lattice[i] = (int*)malloc(sizeof(int)*L);
+		for(j = 0; j < L; j++) {
+			lattice[i][j] = 1;
+		}
+	
+	}
+	int sweeps = 100;
+	for(i = 0; i < sweeps; i++) {
+		sweep(L, lattice, T);
+		printf("%5.6f \n", magnetization(lattice, L));
+	}
+
+	return 0;
 }
 
